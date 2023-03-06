f6e266c991f12336173dd6fefb21e988972747e9
raisss5 <mgbraissa.15@gmail.com>
2023-03-06 11:07:52 +0000

    salvar

--- Salario.java
+++ Salario.java
@@ -10,20 +10,20 @@ public class Salario {
         SalarioAtual = input.nextDouble();
 
 
-        if (SalarioAtual >= 1000 && SalarioAtual <= 2000){
+        if (SalarioAtual >= 1000 && SalarioAtual < 2000){
             SalarioNovo = SalarioAtual * 1.15;
-            System.out.println("seu salário Novo é: " + SalarioAtual);
+            System.out.println("seu salário Novo é: " + SalarioNovo);
             
 
         }
         else if (SalarioAtual >= 2000 && SalarioAtual <= 3000){
             SalarioNovo = SalarioAtual * 1.10;
-            System.out.println("seu salário Novo é: " + SalarioAtual);
+            System.out.println("seu salário Novo é: " + SalarioNovo);
       
         }
         else if (SalarioAtual >= 3000){
             SalarioNovo = SalarioAtual * 1.05;
-            System.out.println("seu salário Novo é: " + SalarioAtual);
+            System.out.println("seu salário Novo é: " + SalarioNovo);
             
         }
     }
